ef54bc41fb5e4d68abc3e9a1779aba4b11325851
P. Warren Groves Jr. <pwjrg28@gmail.com>
2025-05-01 20:59:05 -0500

    log game moves to console and using console to make moves

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-var board = [
-  [5, 4, 3, 2, 1],
-  [],
-  []
-]
+var board = [[5, 4, 3, 2, 1], [], []];
 
-// var moveDisc = function (from, to) {
-//   var firstPeg = board[0];
-//   var secondPeg = board[1];
-//   var thirdPeg = board[2];
+console.log(`--- ${board[0].join(" ")}\n--- ${board[1].join(" ")}\n--- ${board[2].join(" ")}`);
 
-//   if ()
-// }
-  
-console.log(`--- ${board[0].join(" ")}\n---${board[1].join(" ")}\n--- ${board[2].join(" ")}`)
+var moveDisc = function (from, to) {
+  var fromPeg = board[from - 1];
+  var toPeg = board[to - 1];
 
+  if (toPeg.length === 0) {
+    toPeg.push(fromPeg.pop());
+  }
+
+  return console.log(`--- ${board[0].join(" ")}\n--- ${board[1].join(" ")}\n--- ${board[2].join(" ")}`);;
+};
