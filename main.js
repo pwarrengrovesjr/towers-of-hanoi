d0c2895b3ca2fc23b60188e143bf479c84919b6f
P. Warren Groves Jr. <pwjrg28@gmail.com>
2025-05-01 22:18:47 -0500

    game moves in console functioning properly

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var board = [[5, 4, 3, 2, 1], [], []];
 
-console.log(`--- ${board[0].join(" ")}\n--- ${board[1].join(" ")}\n--- ${board[2].join(" ")}`);
+console.log(`Welcome to Towers of Hanoi! \n\n--- ${board[0].join(" ")}\n--- ${board[1].join(" ")}\n--- ${board[2].join(" ")}`);
 
 var moveDisc = function (from, to) {
   var fromPeg = board[from - 1];
@@ -8,7 +8,9 @@ var moveDisc = function (from, to) {
 
   if (toPeg.length === 0) {
     toPeg.push(fromPeg.pop());
-  }
+  } else if (fromPeg[fromPeg.length - 1] < toPeg[toPeg.length - 1]) {
+    toPeg.push(fromPeg.pop());
+  } else {console.log("Invalid move! Can't place a larger disk on top of a smaller disc.")}
 
   return console.log(`--- ${board[0].join(" ")}\n--- ${board[1].join(" ")}\n--- ${board[2].join(" ")}`);;
 };
